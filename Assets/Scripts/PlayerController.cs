0101be163d71e678ec59b898f0e2da840ae12846
Pablo Cáceres Gómez <cgpablo150@gmail.com>
2019-10-23 11:42:11 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController: MonoBehaviour
+{
+    public Rigidbody myRigidBody;
+    public float speed=50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalMovement= Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalMovement, 0f, verticalMovement);
+
+        myRigidBody.AddForce(movement * speed);
+    }
+}
