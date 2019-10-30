6b29114e64556f2e932853a56bacea739b7593db
Shad150 <cgpablo150@gmail.com>
2019-10-30 11:53:21 +0100

    Created Pickup object
    Pickup object created, it dissapears when touched by the ball and incremets score

---
+++ Assets/Scripts/PickupRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupRotation : MonoBehaviour
+{
+
+    public int points = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotation = new Vector3(30,40,45)* Time.deltaTime;       //Vector que recoge la rotacion del objeto * por "Time.deltaTime" (que es el tiempo que pasa en cada frame), para que tenga una rotacion fluida
+        
+        transform.Rotate(rotation);                                     //Se aplica la rotacion al objeto
+    }
+}
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController: MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public Rigidbody myRigidBody;
     public float speed=50f;
+    int score;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0;
     }
 
     // Update is called once per frame
@@ -21,11 +22,22 @@ public class PlayerController: MonoBehaviour
 
     private void FixedUpdate()
     {
-        float horizontalMovement= Input.GetAxis("Horizontal");
-        float verticalMovement = Input.GetAxis("Vertical");
+        
+        
+        float horizontalMovement= Input.GetAxis("Horizontal");                          //Recoge el movimiento horizontal de las flechas del teclado
+        float verticalMovement = Input.GetAxis("Vertical");                             //Recoge el movimiento vertical de las flechas del teclado
 
-        Vector3 movement = new Vector3(horizontalMovement, 0f, verticalMovement);
+        Vector3 movement = new Vector3(horizontalMovement, 0f, verticalMovement);       // Vector que recoge el movimiento
 
-        myRigidBody.AddForce(movement * speed);
+        myRigidBody.AddForce(movement * speed);                                         //La velocidad del objeto. Es el movimiento base * la velocidad extra que le pongo
+    }
+
+    private void OnTriggerEnter(Collider other)         //OnTriggerEnter = Cuando entra en contacto con un collider ... (Collider other) = El other es el objeto con el que choca
+    {
+        if (other.gameObject.CompareTag("Pickup")) {    //Esto sirve para que comparar tags 
+            other.gameObject.SetActive(false);          //Como el tag del objeto es "Pickup" entonces la linea se lleva a cabo
+            score += other.gameObject.GetComponent<PickupRotation>().points;
+            Debug.Log("PUNTUACION= "+ score);
+        }
     }
 }
