f0ed31b3ff344a9f1053af3c845ec709f535b01c
Shad150 <cgpablo150@gmail.com>
2019-11-11 21:34:28 +0100

    Respawner added, started game map
    Spawner added, suddenly stopped working
    Started building map enviroment

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,27 +6,29 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody myRigidBody;
-    public float speed=50f;
+    public float speed = 50f;
     public Text scoreText;
+    public GameObject resPosition;  //Objeto que recoge la posición del respawner. Arrastrado el respawner al player para asignarlo.
     int score;
 
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
+        transform.position = resPosition.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
-        
-        
-        float horizontalMovement= Input.GetAxis("Horizontal");                          //Recoge el movimiento horizontal de las flechas del teclado
+
+
+        float horizontalMovement = Input.GetAxis("Horizontal");                          //Recoge el movimiento horizontal de las flechas del teclado
         float verticalMovement = Input.GetAxis("Vertical");                             //Recoge el movimiento vertical de las flechas del teclado
 
         Vector3 movement = new Vector3(horizontalMovement, 0f, verticalMovement);       // Vector que recoge el movimiento
@@ -36,19 +38,18 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)         //OnTriggerEnter = Cuando entra en contacto con un collider ... (Collider other) = El other es el objeto con el que choca       [REFERENCIA]
     {
-        if (other.gameObject.CompareTag("Pickup")) {    //Esto sirve para que comparar tags 
+
+        if (other.gameObject.CompareTag("Pickup"))
+        {    //Esto sirve para que comparar tags 
+
             other.gameObject.SetActive(false);          //Como el tag del objeto es "Pickup" entonces la linea se lleva a cabo
             score += other.gameObject.GetComponent<PickupRotation>().points;
             scoreText.text = "SCORE: " + score;
         }
-    }
-
-    private void OnTriggerEnter(Collider other)         //OnTriggerEnter = Cuando entra en contacto con un collider ... (Collider other) = El other es el objeto con el que choca       [REFERENCIA]
-    {
-        if (other.gameObject.CompareTag("Lava"))
-        {    
-            
-            
+        
+        if (other.gameObject.CompareTag("Death"))
+        {
+            transform.position = resPosition.transform.position;
         }
     }
 }
